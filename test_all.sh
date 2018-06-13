9c71acf48bfb0395f5f1670a4f52adb9462d9baa
mbohun <martin.bohun@gmail.com>
2018-06-13 14:34:49 +1000

    FIX: Process_NSL_Source_Data.sh is (officially) disabled at the moment.

--- test_all.sh
+++ test_all.sh
@@ -2,7 +2,6 @@
 
 ./Load_from_SFTP.sh \
     && ./Process_TaxxaS_Source_Data.sh \
-#    && ./Process_NSL_Source_Data.sh \
     && ./Combine_Taxonomies.sh \
     && ./Import_Taxonomy.sh \
     && ./Swap_Cores.sh
