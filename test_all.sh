e3cfdd2baefe0b144ab252d0092b84a1a752b6fe
mbohun <martin.bohun@gmail.com>
2018-06-13 13:29:38 +1000

    add a simple wrapper/runner script that runs all the scripts in the same order as they are executed by jenkins

---
+++ test_all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./Load_from_SFTP.sh \
+    && ./Process_TaxxaS_Source_Data.sh \
+#    && ./Process_NSL_Source_Data.sh \
+    && ./Combine_Taxonomies.sh \
+    && ./Import_Taxonomy.sh \
+    && ./Swap_Cores.sh
