1e2a9d6e9414203a78329617e8852a36db6138ee
mbohun <martin.bohun@gmail.com>
2018-06-22 16:57:56 +1000

    FIX/CLEANUP/ENHANCEMENT: pretty/color messages

--- test_all.sh
+++ test_all.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-./Load_from_SFTP.sh \
-    && ./Process_TaxxaS_Source_Data.sh \
-    && ./Combine_Taxonomies.sh \
-    && ./Import_Taxonomy.sh \
-    && ./Swap_Cores.sh
+set -e
+
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+ag_bie_scripts="Load_from_SFTP.sh Process_TaxxaS_Source_Data.sh Combine_Taxonomies.sh Import_Taxonomy.sh Swap_Cores.sh"
+
+for script in ${ag_bie_scripts}
+do
+    echo "${green}${script}${reset}"
+    ./${script}
+    echo
+done
+
+exit 0
