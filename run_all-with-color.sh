b58abc249489639d7960d2d99491f409a5bc6476
mbohun <martin.bohun@gmail.com>
2018-07-02 13:30:58 +1000

    CLEANUP: run_all-with-color.sh / color *DOES REQUIRE* the env var xterm-256color to be set, HOWEVER that is *NOT* the case (default) in the Jenkins web GUI, and that was breaking the full-automated installation.

--- run_all-with-color.sh
+++ run_all-with-color.sh
@@ -2,11 +2,14 @@
 
 set -e
 
+green=`tput setaf 2`
+reset=`tput sgr0`
+
 ag_bie_scripts="Load_from_SFTP.sh Process_TaxxaS_Source_Data.sh Combine_Taxonomies.sh Import_Taxonomy.sh Swap_Cores.sh"
 
 for script in ${ag_bie_scripts}
 do
-    echo "RUNNING SCRIPT: ${script}"
+    echo "${green}${script}${reset}"
     ./${script}
     echo
 done
