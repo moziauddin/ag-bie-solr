8292dc0838a9d4f2922b7828872fc2ca48dcc693
moziauddin <moziauddin@gmail.com>
2019-03-12 16:32:48 +1100

    Disabled the NSL Data integration

--- Combine_Taxonomies.sh
+++ Combine_Taxonomies.sh
@@ -30,7 +30,7 @@ rm "${combined}"/*
 
 # Combine DwCAs
 cd "${combineDir}" || bail "Uable to get to process directory ${combineDir}"
-./build-combined.sh -c "${configDir}/taxxas-taxon-config.json" -w "${workDir}" -o "${combined}"  "${taxxasDir}/DwC/" "${nslICNDir}/DwC/" "${nslICZNDir}/DwC" "${nslICNPDir}/DwC" "${nslICVCNDir}/DwC"
+./build-combined.sh -c "${configDir}/taxxas-taxon-config.json" -w "${workDir}" -o "${combined}"  "${taxxasDir}/DwC/" #"${nslICNDir}/DwC/" "${nslICZNDir}/DwC" "${nslICNPDir}/DwC" "${nslICVCNDir}/DwC"
 #./build-combined.sh -c "${configDir}/taxxas-taxon-config.json" -w "${workDir}" -o "${combined}"  "${taxxasDir}/DwC/"
 if [ $? -ne 0 ]; then
  bail "Unable to combine taxonomies"
