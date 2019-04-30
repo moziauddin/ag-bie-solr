64b4acf36c27eae3e1fc1049e9774ea6bcee6586
moziauddin <moziauddin@gmail.com>
2019-04-30 11:22:00 +1000

    Added two jobs for ANBG Vascular plants and Lichens to the run_all.sh script

--- run_all.sh
+++ run_all.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-ag_bie_scripts="Load_from_SFTP.sh Process_TaxxaS_Source_Data.sh Process_NSL_Source_Data_ICN.sh Process_NSL_Source_Data_ICNP.sh Process_NSL_Source_Data_ICVCN.sh Process_NSL_Source_Data_ICZN.sh Process_ANBG_Source_Data_Mosses.sh Combine_Taxonomies.sh Import_Taxonomy.sh Swap_Cores.sh"
+ag_bie_scripts="Load_from_SFTP.sh Process_TaxxaS_Source_Data.sh Process_NSL_Source_Data_ICN.sh Process_NSL_Source_Data_ICNP.sh Process_NSL_Source_Data_ICVCN.sh Process_NSL_Source_Data_ICZN.sh Process_ANBG_Source_Data_Mosses.sh Process_ANBG_Source_Data_Lichens.sh Process_ANBG_Source_Data_Vascular.sh Combine_Taxonomies.sh Import_Taxonomy.sh Swap_Cores.sh"
 
 for script in ${ag_bie_scripts}
 do
