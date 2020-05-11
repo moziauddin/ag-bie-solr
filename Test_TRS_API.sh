e449ef70c51d2aeef32843228ad33e30b5b17641
Mo <moziauddin@gmail.com>
2020-05-12 09:12:37 +1000

    Added a new script to run tests

---
+++ Test_TRS_API.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -x
+bail() {
+  echo 1>&2 $*
+  exit 1
+}
+
+datestamp=`date +'%Y%m%d'`
+echo "${datestamp}"
+
+# Run Test Python Script
+chmod +x ./trs-api-tests.py
+python ./trs-api-tests.py localhost
